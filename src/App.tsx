ee9ead0f012f84c12ee8fb2262fc1cd82a6e6f71
Ricky004 <tridipdam001@gmail.com>
2025-04-08 11:20:52 +0530

    add user selection page based on his type

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 // App.tsx
 import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
-import ChatDemo from './chatDemo';
+import ChatDemo from './ChatDemo';
+import Signup from './Signup';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<Signup />} />
       <Route path="/chatDemo" element={<ChatDemo />} />
     </Routes>
   );
